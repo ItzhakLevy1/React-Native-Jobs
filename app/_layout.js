468699f1da9f22c2fabc263e78b260217a0890ab
Itzhak Levy <itzhaklevydev@gmail.com>
2025-03-08 14:05:12 +0200

    Modify the Welcome.jsx content to display jobs types options and implement the search bar (with out importing fonts)

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,26 @@
 import { Stack } from "expo-router";
 
+import { useCallback, useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
 const Layout = () => {
-  return <Stack />;
-}
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, []);
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
 
 export default Layout;
